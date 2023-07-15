7b63ffa89388668383dfda8512243ffae34e2191
giuseppe-pagliaro <giuseppepagliaro.vf@gmail.com>
2023-07-15 12:31:08 +0200

    Commons Set Up

---
+++ HermoCommons/ItemDatas.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace HermoCommons
+{
+    public class ItemDatas : ICloneable
+    {
+        public ItemDatas()
+        {
+            Id = 0;
+        }
+
+        public int Id { get; set; }
+
+        public ItemDatas Clone() { return (ItemDatas)this.MemberwiseClone(); }
+        object ICloneable.Clone() { return Clone(); }
+
+        public String ClassNameToString()
+        {
+            String rawName = this.GetType().Name;
+            return Regex.Replace(rawName, @"(\p{Lu})", " $1").TrimStart();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (this == obj)
+            {
+                return true;
+            }
+
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            ItemDatas other = (ItemDatas)obj;
+
+            return other.Id == Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+    }
+}
