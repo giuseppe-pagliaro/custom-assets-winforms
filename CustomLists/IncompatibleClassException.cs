de89d894cac7203c2c213ef7c247fb4bacb52f23
giuseppe-pagliaro <giuseppepagliaro.vf@gmail.com>
2023-07-15 12:13:54 +0200

    Initial setup.

--- CustomLists/IncompatibleClassException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CustomLists
-{
-    public class IncompatibleClassException : Exception
-    {
-        public IncompatibleClassException()
-            : base("The class you provided isn't or doesn't extend \"CustomItemManagers.FieldsForm\".") { }
-    }
-}
